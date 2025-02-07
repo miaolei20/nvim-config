fc6c154c881e5ae4d3e926d1ab7d1c629c14e788
miaolei20 <ml20061023@outlook.com>
2025-02-07 14:05:52 +0800

    fix

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,15 @@
 return {
     {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = true,
-        opts = {},
-    },
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({
+        check_ts = true, -- 使用 Treesitter 检查上下文
+        ts_config = {
+          lua = { "string" }, -- 在 Lua 的字符串中不启用
+          javascript = { "template_string" },
+        },
+      })
+    end,
+  },
 }
 
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,38 +1,38 @@
 -- lazy.nvim插件配置
 return {
-  {
-    "akinsho/toggleterm.nvim",
-    version = "*",
-     keys = {
-    { '<leader>tt', desc = 'Toggle基础终端' },
-    { '<leader>tf', desc = 'Toggle浮动终端' },
-  },
-    config = function()
-      require("toggleterm").setup({
-        size = function(term)
-          return (term.direction == "horizontal") and 15
-            or (term.direction == "vertical") and vim.o.columns * 0.4
-            or 20
-        end,
-        persist_size = false,
-        direction = "float", -- 默认使用悬浮终端
-        close_on_exit = true,
-        highlights = {
-          FloatBorder = { link = "ToggleTermBorder" },
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        keys = {
+            { "<leader>tt", desc = "Toggle基础终端" },
+            { "<leader>tf", desc = "Toggle浮动终端" },
         },
-        float_opts = {
-          border = "rounded", -- 圆角边框
-          winblend = 10,      -- 透明度混合
-          width = math.floor(vim.o.columns * 0.8),
-          height = math.floor(vim.o.lines * 0.8),
-        }
-      })
-      -- 按键映射
-      local function map(mode, lhs, rhs, desc)
-      vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
-    end
-       map('n', '<leader>tt', '<cmd>ToggleTerm direction=horizontal<cr>', '水平终端')
-    map('n', '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', '浮动终端')
-    end
-  }
+        config = function()
+            require("toggleterm").setup({
+                size = function(term)
+                    return (term.direction == "horizontal") and 15
+                        or (term.direction == "vertical") and vim.o.columns * 0.4
+                        or 20
+                end,
+                persist_size = false,
+                direction = "float", -- 默认使用悬浮终端
+                close_on_exit = true,
+                highlights = {
+                    FloatBorder = { link = "ToggleTermBorder" },
+                },
+                float_opts = {
+                    border = "rounded", -- 圆角边框
+                    winblend = 10, -- 透明度混合
+                    width = math.floor(vim.o.columns * 0.8),
+                    height = math.floor(vim.o.lines * 0.8),
+                },
+            })
+            -- 按键映射
+            local function map(mode, lhs, rhs, desc)
+                vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+            end
+            map("n", "<leader>tt", "<cmd>ToggleTerm direction=horizontal<cr>", "水平终端")
+            map("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", "浮动终端")
+        end,
+    },
 }
